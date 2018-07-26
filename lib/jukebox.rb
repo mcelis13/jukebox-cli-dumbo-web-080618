aeefe1aac15d1d35e762e0e7af04ab18074bd450
Madeline Celis <madelinebcelis@gmail.com>
2018-07-26 18:16:14 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,6 +30,9 @@ def play(songs)
   puts 'Please enter a song name or number:'
   user_input = gets.chomp
 
+  if user_input.length < 2
+  end
+
   if user_input.class == String && songs.include?(user_input)
     puts "Playing #{user_input}"
     return
