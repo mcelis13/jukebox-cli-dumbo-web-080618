1862ca87ca97442e1522752d2a1db86877f99446
Madeline Celis <madelinebcelis@gmail.com>
2018-07-26 18:39:57 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   puts 'Please enter a song name or number:'
   user_input = gets.chomp
 
-  if user_input == Fixnum
+  if user_input.to_i == Fixnum
     int = user_input.to_i
     index = int - 1
     if index <= songs.size
